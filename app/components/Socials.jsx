f21fe498e50bcb179d40ece856146234c482d1da
Terino16 <123401184+Terino16@users.noreply.github.com>
2023-06-19 00:03:02 +0530

    second commit

--- app/components/Socials.jsx
+++ app/components/Socials.jsx
@@ -1,9 +1,28 @@
+'use client';
 import React from 'react'
-
+import linkdin from '../../public/linkdin.webp'
+import insta from '../../public/insta.webp'
+import github from '../../public/github.webp'
+import Image from 'next/image'
+import Link from 'next/link';
 const Socials = () => {
   return (
-    <div className='flex '>
-
+    <div className='flex xl:space-x-24 '>
+     <span className='rounded-full animate-bounce p-3 bg-blue-400 bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-50'>
+      <Link href="https://www.linkedin.com/in/anubhav-dube-b02557236/" target='_blank' alt="sc">
+      <Image src={linkdin} alt="Linkdinlogo" width={50} height={50}/>
+      </Link>
+     </span>
+     <span className='rounded-full animate-bounce p-3 bg-red-500 bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-50'>
+      <Link href="https://www.instagram.com/anubhav_160/" target='_blank' alt="sc">
+      <Image src={insta} alt="Linkdinlogo" width={50} height={50}/>
+      </Link>
+     </span>
+     <span className='rounded-full animate-bounce p-3 bg-yellow-500 bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-50'>
+      <Link href="https://github.com/Terino16" target='_blank' alt="sc">
+      <Image src={github} alt="Linkdinlogo" width={50} height={50}/>
+      </Link>
+     </span>
     </div>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
-
+import Socials from './components/Socials'
 import Hero from '@/app/components/Hero'
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between xl:p-24 sm:p-1  ">
+    <main className="flex min-h-screen flex-col items-center  xl:p-24 sm:p-1  ">
     <Hero/>
-   
+   <Socials/>
     </main>
   )
 }
